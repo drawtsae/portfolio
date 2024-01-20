a6885f495993b347a58980e39c7ed2b5999a57d2
drawtsae <donghuongit@gmail.com>
2024-01-21 01:30:15 +0700

    .

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -60,7 +60,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <img
-              src="/images/avatar.png"
+              src="images/avatar.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
